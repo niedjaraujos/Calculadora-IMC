462d6edf4dae271f89391e9258bacf8289e38d7e
niedjaraujos <163071319+niedjaraujos@users.noreply.github.com>
2025-01-22 17:39:19 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+
+
+const botao = document.querySelector('button');
+const inputAltura = document.querySelector('#altura');
+const inputPeso = document.querySelector('#peso');
+
+botao.addEventListener('click', () => {
+    let altura = inputAltura.value;
+    let peso = inputPeso.value;
+
+    let imc = parseInt(peso / (altura * altura))
+
+    if (imc <= 19) {
+        document.querySelector('.resultado').textContent = `Seu imc é ${imc}. Magreza`
+    }
+    else if (imc <= 25) {
+        document.querySelector('.resultado').textContent = `Seu imc é ${imc}. Normal`
+    }
+    else if (imc <= 29) {
+        document.querySelector('.resultado').textContent = `Seu imc é ${imc}. Sobrepeso grau I`
+    }
+    else if (imc <= 39) {
+        document.querySelector('.resultado').textContent = `Seu imc é ${imc}. Obesidade grau II`
+    }
+    else {
+        document.querySelector('.resultado').textContent = `Seu imc é ${imc}. Obesidade grave grau III`
+    }
+
+    console.log(imc)
+}) 
